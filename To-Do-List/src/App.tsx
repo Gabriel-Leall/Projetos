1a655588c5c6fea18952c32e069f58b6785e730e
Gabriel-Leall <dragonleal29@gmail.com>
2025-05-27 22:18:16 -0300

    piloto do projeto: To-Do-List

---
+++ To-Do-List/src/App.tsx
@@ -0,0 +1,17 @@
+// src/App.tsx
+
+
+function App() {
+  // O conteúdo da função App pode ficar vazio,
+  // o importante é o que está dentro do return.
+
+  return (
+    <div className="min-h-screen bg-green-900 flex items-center justify-center">
+      <h1 className="text-3xl font-bold underline text-green-500 p-6 bg-gray-800 rounded-lg shadow-lg">
+      aaaaaaaaaaaaaaaaaaaaaaaaaa
+      </h1>
+    </div>
+  );
+}
+
+export default App; // Mantém esta linha
